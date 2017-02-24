7c3324f96abec2f3a6c28a6faba618e96809bef5
Jake Iwen <iwenx006@umn.edu>
2017-02-24 02:05:37 -0600

    basic feed

--- slack-isolate/index.jsx
+++ slack-isolate/index.jsx
@@ -1,14 +1,35 @@
-/*
-Creating a component
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import Tooltip from './react-tooltip.min.js';
+// var Tooltip = require('rc-tooltip');
 
-The most simple component has a `render` method that returns some
-JSX. `props` are attributes that are passed into the component
-when you instantiate it.
+const text = <span>{this.props.userData.email}</span>;
+const styles = {
+  display: 'inline-block',
+  lineHeight: '40px',
+  height: '40px',
+  width: '80px',
+  textAlign: 'center',
+  background: '#f6f6f6',
+  marginRight: '1em',
+  marginBottom: '1em',
+  borderRadius: '6px',
+};
 
-One caveat is that `render` must return a single parent element;
-you can't return multiple adjacent JSX tags but must wrap them
-in one parent element.
-*/
+var userInfo = React.createClass({
+  render() {
+    return (
+      <div>
+        <Tooltip
+          placement="right"
+          overlay={text}
+          arrowContent={<div className="rc-tooltip-arrow-inner"></div>}
+        >
+          <a href="#" style={styles}>左边</a>
+        </Tooltip>
+      </div>);
+  },
+});
 
 var MessageItem = React.createClass({
   render: function() {
@@ -35,7 +56,7 @@ var MessageList = React.createClass({
     var req = buildQuery('users.list');
     var usersById = {};
     $.getJSON(req, function(userList) {
-      console.log('userList', userList.members);
+      // console.log('userList', userList.members);
       //create hash key
       for (var i = 0; i < userList.members.length; i++) {
         usersById[userList.members[i].id] = userList.members[i];
@@ -48,7 +69,7 @@ var MessageList = React.createClass({
         for (var i = 0; i < data.messages.length; i++) {
           data.messages[i].userData = usersById[data.messages[i].user];
         }
-        console.log('messages', data.messages);
+        // console.log('messages', data.messages);
         self.setState({
           msgData: data.messages.sort(dynamicSort("-ts"))
         });
@@ -57,7 +78,6 @@ var MessageList = React.createClass({
 
  		function buildQuery (method, arg) {
  			arg = arg || '';
- 			console.log('arg', arg);
  			var token = '?token=xoxp-23646916496-23649242352-143236957938-e948672ba71d8389d3485803d2a07a15';
  			var query = 'https://slack.com/api/';
  			query += method + token + arg + '&pretty=1';
@@ -66,15 +86,17 @@ var MessageList = React.createClass({
   },
   render: function () {
     var messages = this.state.msgData.map(function (item, index) {
-      console.log('item', item.userData.);
-      return (
-        <MessageItem
-          key={index}
-          ts={item.ts}
-          user={item.user}
-          text={item.text}
-        />
-      );
+      // console.log('item', item.userData);
+      if(item.userData && item.userData.name) {
+        return (
+          <MessageItem
+            key={index}
+            ts={item.ts}
+            user={item.userData.name}
+            text={item.text}
+          />
+        );
+      }
     });
     return (
       <section>
@@ -112,4 +134,4 @@ function dynamicSort(property) {
 }
 
 
-ReactDOM.render(<MessageList />, document.getElementById('root'));
+ReactDOM.render(<Test />, document.getElementById('root'));
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,9 @@
 var React = require( 'react' );
 var $ = require( 'jquery' );
+var Tooltip = require('rc-tooltip');
+import 'rc-tooltip/assets/bootstrap_white.css';
+// var Tooltip = require('../../slack-isolate/react-tooltip.min.js');
+
 
 var QuoteDisplay = require( './quote-display.tsx' ).default,
 	InfoPanel = require( './info-panel.jsx' );
@@ -8,13 +12,54 @@ import { IState } from '../store/reducer';
 import { showInfoPanel } from '../store/actions';
 import { areNewFeaturesAvailable } from '../store/selectors';
 import { connect } from 'react-redux';
+// import Tooltip from '../../slack-isolate/react-tooltip.min.js';
+
+
+
+const styles = {
+  display: 'inline-block',
+  // lineHeight: '40px',
+  // height: '40px',
+  // width: '80px',
+  textAlign: 'center',
+  // background: '#f6f6f6',
+	textDecoration: 'underline',
+  marginLeft: '1em',
+  marginBottom: '.1em',
+  // borderRadius: '6px',
+};
+
+var SlackLogo = React.createClass({
+  render: function() {
+		return (
+			<div className='logo'>
+				<img src='https://lh3.googleusercontent.com/CzlsZP3xUHeX3HAGdZ2rL9mK6_C-6T1-YWeBeM8nB3ilmfPSBHCFx4-UbQr8MnQms3d9=w300' height='40' />
+				<span>SlackFeed</span>
+			</div>
+		)
+	}
+
+});
 
 var MessageItem = React.createClass({
   render: function() {
+		var text =  <div>
+								  <img src={this.props.profile.image_192} /><br />
+								  <span>{this.props.profile.real_name}</span><br />
+								  <span>{this.props.profile.email}</span>
+								</div>;
+			console.log('text', text);
+			console.log('imgsrc', this.props.profile.image_192);
     return (
       <div>
         <p>{timeConverter(this.props.ts)}</p>
-        <p>{this.props.user}</p>
+					<Tooltip
+						placement="right"
+						overlay={text}
+						arrowContent={<div className="rc-tooltip-arrow-inner"></div>}
+					>
+						<a href="#" style={styles}>{this.props.user}</a>
+					</Tooltip>
         <p>{this.props.text}</p>
       </div>
     )
@@ -29,19 +74,33 @@ var MessageList = React.createClass({
     }
   },
   componentDidMount: function() {
-		var request = buildQuery('channels.history', '&channel=C0PK0SZDW');
-    console.log(request);
+    const self = this;
     //get list of 100 messages from a slack channel
- 		$.getJSON(request, function(data) {
- 			console.log('data', data);
- 			this.setState({
- 				msgData: data.messages.sort(dynamicSort("ts"))
- 			});
- 		}.bind(this));
+    var req = buildQuery('users.list');
+    var usersById = {};
+    $.getJSON(req, function(userList) {
+      // console.log('userList', userList.members);
+      //create hash key
+      for (var i = 0; i < userList.members.length; i++) {
+        usersById[userList.members[i].id] = userList.members[i];
+      }
+    }).then(function() {
+      req = buildQuery('channels.history', '&channel=C0PK0SZDW');
+      //get message list
+      $.getJSON(req, function(data) {
+        //append user info to each message
+        for (var i = 0; i < data.messages.length; i++) {
+          data.messages[i].userData = usersById[data.messages[i].user];
+        }
+        // console.log('messages', data.messages);
+        self.setState({
+          msgData: data.messages.sort(dynamicSort("-ts"))
+        });
+      }.bind(this));
+    });
 
  		function buildQuery (method, arg) {
  			arg = arg || '';
- 			console.log('arg', arg);
  			var token = '?token=xoxp-23646916496-23649242352-143236957938-e948672ba71d8389d3485803d2a07a15';
  			var query = 'https://slack.com/api/';
  			query += method + token + arg + '&pretty=1';
@@ -50,14 +109,18 @@ var MessageList = React.createClass({
   },
   render: function () {
     var messages = this.state.msgData.map(function (item, index) {
-      return (
-        <MessageItem
-          key={index}
-          ts={item.ts}
-          user={item.user}
-          text={item.text}
-        />
-      );
+      // console.log('item', item.userData);
+      if(item.userData && item.userData.name) {
+        return (
+          <MessageItem
+            key={index}
+            ts={item.ts}
+            user={item.userData.name}
+            text={item.text}
+						profile={item.userData.profile}
+          />
+        );
+      }
     });
     return (
       <section>
@@ -68,19 +131,17 @@ var MessageList = React.createClass({
 
 });
 
-
 var NewsFeedEradicator = React.createClass( {
 	render: function() {
 		var quoteDisplay = null;
 		if ( this.props.quotesVisible === true ) {
 			quoteDisplay = (
-				<div>
+				<div className="feedField">
+					<SlackLogo />
 					<MessageList />
-					<QuoteDisplay />
 				</div>
 			);
 		}
-
 		let newFeatureLabel = null;
 		if ( this.props.newFeaturesAvailable ) {
 			newFeatureLabel = <span className="nfe-label nfe-new-features">New Features!</span>;
